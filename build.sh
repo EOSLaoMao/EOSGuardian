bba291089564b6817e5173b69a5de8aff38823b2
iCell <icell.vip@gmail.com>
2019-02-18 22:41:02 +0800

    refactor: update build script to adapt 1.4.0 image

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-PROJECT=eosguardian
-CONTAINER=$PROJECT-eos-dev
+IMG_NAME=eoslaomao/eos-dev:1.4.0
+PROJECT_NAME=eosguardian
+WORKING_DIR=eosguardian
 
-docker ps | grep $CONTAINER
+docker ps | grep $PROJECT_NAME-eos-dev
+
 if [ $? -ne 0 ]; then
     echo "Run eos dev env "
-    docker run --name $CONTAINER -dit --rm -v  `(pwd)`:/$PROJECT eoslaomao/eos-dev:1.2.3
+    docker run --name $PROJECT_NAME-eos-dev -dit --rm -v  `(pwd)`:/$PROJECT_NAME $IMG_NAME
 fi
 
-docker exec $CONTAINER eosiocpp -g /$PROJECT/$PROJECT.abi /$PROJECT/src/$PROJECT.cpp
-docker exec $CONTAINER eosiocpp -o /$PROJECT/$PROJECT.wast /$PROJECT/src/$PROJECT.cpp 
-docker cp ../$PROJECT nodeosd:/
+docker exec $PROJECT_NAME-eos-dev eosio-cpp --contract $PROJECT_NAME \
+    -abigen /$WORKING_DIR/src/eosguardian.cpp \
+    -o $PROJECT_NAME.wasm \
+    -I /contracts/eosio.token/include \
+    -I /contracts/eosio.system/include \
+
+docker exec $PROJECT_NAME-eos-dev cp /$PROJECT_NAME.abi /$PROJECT_NAME.wasm /$PROJECT_NAME/
